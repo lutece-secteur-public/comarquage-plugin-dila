033cb13d6e3d2cb8c4e1cb1b68f3a8b605fe1107
SLO <SLO@bab10101-e421-0410-a517-8ce0973de3ef>
2013-11-08 11:47:22 +0000

    DILA-6 Log error when not finding organism file.
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/plugins/cms/plugin-dila@53820 bab10101-e421-0410-a517-8ce0973de3ef

--- src/java/fr/paris/lutece/plugins/dila/service/impl/DilaPivotLocalService.java
+++ src/java/fr/paris/lutece/plugins/dila/service/impl/DilaPivotLocalService.java
@@ -8,20 +8,9 @@ import fr.paris.lutece.plugins.dila.utils.constants.DilaConstants;
 import fr.paris.lutece.portal.service.util.AppLogService;
 import fr.paris.lutece.portal.service.util.AppPropertiesService;
 
-import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.lang.StringUtils;
-
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-
-import org.xml.sax.SAXException;
-
 import java.io.File;
 import java.io.IOException;
 import java.io.Serializable;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,13 +18,22 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang.StringUtils;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
 
 /**
  * This class provides a method to find XML filenames according to code pivot
- *
+ * 
  */
 public class DilaPivotLocalService implements IDilaPivotLocalService, Serializable
 {
+    private static final String ERROR_PIVOT_NOT_FOUND = "Unable to find pivot file ";
     private static final String TAG_PIVOT_LOCAL = "PivotLocal";
     private static final String ATTR_ID = "id";
     private static final String TAG_ORGANISME = "Organisme";
@@ -51,22 +49,22 @@ public class DilaPivotLocalService implements IDilaPivotLocalService, Serializab
     @Override
     public List<String> findByCodePivot( String strCodePivot )
     {
-        List<String> fileNames = new ArrayList<String>(  );
+        List<String> fileNames = new ArrayList<String>( );
         String insee = AppPropertiesService.getProperty( DilaConstants.PROPERTY_INSEE );
         String townDirectory = AppPropertiesService.getProperty( DilaConstants.PROPERTY_XML_DIRECTORY_LOCALES_TOWN );
-        File town = new File( townDirectory + insee.substring( 0, 2 ) + File.separator + insee +
-                DilaConstants.XML_EXTENSION );
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance(  );
+        File town = new File( townDirectory + insee.substring( 0, 2 ) + File.separator + insee
+                + DilaConstants.XML_EXTENSION );
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance( );
 
         try
         {
-            DocumentBuilder builder = factory.newDocumentBuilder(  );
+            DocumentBuilder builder = factory.newDocumentBuilder( );
             Document document = builder.parse( town );
-            document.getDocumentElement(  ).normalize(  );
+            document.getDocumentElement( ).normalize( );
 
             NodeList pivots = document.getElementsByTagName( TAG_TYPE_ORGANISME );
 
-            for ( int i = 0; i < pivots.getLength(  ); i++ )
+            for ( int i = 0; i < pivots.getLength( ); i++ )
             {
                 Element pivotNode = (Element) pivots.item( i );
 
@@ -74,7 +72,7 @@ public class DilaPivotLocalService implements IDilaPivotLocalService, Serializab
                 {
                     NodeList listOrganisms = pivotNode.getElementsByTagName( TAG_ORGANISME );
 
-                    for ( int j = 0; j < listOrganisms.getLength(  ); j++ )
+                    for ( int j = 0; j < listOrganisms.getLength( ); j++ )
                     {
                         Element organism = (Element) listOrganisms.item( j );
 
@@ -108,25 +106,24 @@ public class DilaPivotLocalService implements IDilaPivotLocalService, Serializab
      * {@inheritDoc}
      */
     @Override
-    public Document insertPivots( DocumentBuilder builder, Document document )
-        throws SAXException, IOException
+    public Document insertPivots( DocumentBuilder builder, Document document ) throws SAXException, IOException
     {
         NodeList pivots = document.getElementsByTagName( TAG_PIVOT_LOCAL );
 
-        for ( int i = 0; i < pivots.getLength(  ); i++ )
+        for ( int i = 0; i < pivots.getLength( ); i++ )
         {
             Node pivotNode = pivots.item( i );
 
             if ( pivotNode != null )
             {
-                String codePivot = pivotNode.getTextContent(  );
+                String codePivot = pivotNode.getTextContent( );
                 pivotNode.setTextContent( StringUtils.EMPTY );
 
                 List<String> localOrganisms = this.findByCodePivot( codePivot );
 
                 if ( CollectionUtils.isEmpty( localOrganisms ) )
                 {
-                    pivotNode.getParentNode(  ).removeChild( pivotNode );
+                    pivotNode.getParentNode( ).removeChild( pivotNode );
                     i--;
                 }
                 else
@@ -134,18 +131,25 @@ public class DilaPivotLocalService implements IDilaPivotLocalService, Serializab
                     for ( String organism : localOrganisms )
                     {
                         String[] splitOrganisme = organism.split( "-" );
-                        String directoryOrganisme = AppPropertiesService.getProperty( DilaConstants.PROPERTY_XML_DIRECTORY_LOCALES_ORGANISMS );
-                        File pivotFile = new File( directoryOrganisme + splitOrganisme[1].substring( 0, 2 ) +
-                                File.separator + organism + DilaConstants.XML_EXTENSION );
+                        String directoryOrganisme = AppPropertiesService
+                                .getProperty( DilaConstants.PROPERTY_XML_DIRECTORY_LOCALES_ORGANISMS );
+                        String fileName = directoryOrganisme + splitOrganisme[1].substring( 0, 2 ) + File.separator
+                                + organism + DilaConstants.XML_EXTENSION;
+                        File pivotFile = new File( fileName );
+                        if ( !pivotFile.exists( ) )
+                        {
+                            AppLogService.error( ERROR_PIVOT_NOT_FOUND + fileName );
+                            continue;
+                        }
                         Document documentPivot = builder.parse( pivotFile );
 
-                        Node pivot = documentPivot.getFirstChild(  );
+                        Node pivot = documentPivot.getFirstChild( );
                         Node importedPivot = document.importNode( pivot, true );
                         pivotNode.appendChild( importedPivot );
                     }
                 }
 
-                document.getDocumentElement(  ).normalize(  );
+                document.getDocumentElement( ).normalize( );
             }
         }
 
