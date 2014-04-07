e5e8413615fdf6b3a8236348db74d8288aa6fb21
RPU <RPU@bab10101-e421-0410-a517-8ce0973de3ef>
2014-04-07 13:00:42 +0000

    DILA-23 - Breacrumb from local folder display "null"
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/plugins/cms/plugin-dila@65560 bab10101-e421-0410-a517-8ce0973de3ef

--- src/java/fr/paris/lutece/plugins/dila/web/LocalCardJspBean.java
+++ src/java/fr/paris/lutece/plugins/dila/web/LocalCardJspBean.java
@@ -33,6 +33,16 @@
  */
 package fr.paris.lutece.plugins.dila.web;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang.StringUtils;
+
 import fr.paris.lutece.plugins.dila.business.enums.ActionTypeEnum;
 import fr.paris.lutece.plugins.dila.business.fichelocale.dto.AudienceDTO;
 import fr.paris.lutece.plugins.dila.business.fichelocale.dto.LocalCardChapterDTO;
@@ -61,16 +71,6 @@ import fr.paris.lutece.portal.web.pluginaction.IPluginActionResult;
 import fr.paris.lutece.util.ReferenceList;
 import fr.paris.lutece.util.datatable.DataTableManager;
 import fr.paris.lutece.util.html.HtmlTemplate;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-
-import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.lang.StringUtils;
 
 
 /**
@@ -653,7 +653,7 @@ public class LocalCardJspBean extends PluginAdminPageJspBean
         {
             _cardBean.getLocalDTO( ).setBreadCrumb(
                     _cardBean.getLocalParentFolder( ).getLocalDTO( ).getBreadCrumb( ) + ";"
-                            + _cardBean.getLocalParentFolder( ).getId( ) );
+                            + _cardBean.getLocalParentFolder( ).getLocalDTO( ).getId( ) );
         }
         else
         {
